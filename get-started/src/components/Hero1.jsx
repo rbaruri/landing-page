c0f8c61a05ae9c46bf62a91af29fb06d368459f8
Rajasree Baruri <rbaruri2002@gmail.com>
2023-10-13 01:52:13 +0530

    mockups

--- get-started/src/components/Hero1.jsx
+++ get-started/src/components/Hero1.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const Hero1 = () => {
   return (
-    <div className="hero min-h-screen bg-base-200">
+    <div className="hero min-h-screen bg-base-200 px-20">
       <div className="hero-content flex-col lg:flex-row">
         <img
-          src="/images/stock/photo-1635805737707-575885ab0820.jpg"
+          src="https://i.imgur.com/Xqx80C5.jpg"
           className="max-w-sm rounded-lg shadow-2xl"
         />
         <div>
-          <h2 className="text-5xl font-bold">Streamlined Task Management</h2>
-          <p className="py-6">
+          <h2 className="text-5xl font-bold mx-10">Streamlined Task Management</h2>
+          <p className="py-6 mx-10">
             Effortlessly add, edit, and organize
             tasks in the productivity planner for a seamless workflow.
             Categorize tasks as "To-Do," "Doing," and "Done" with ease.
